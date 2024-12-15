3a2d83cff5c0a44679cabd53f316fd9a7eee5313
OmgCopito95 <july1995bolt@gmail.com>
2024-12-15 18:19:11 -0300

    rutas

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -2,18 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import Layout from "../components/Layout/Layout";
 import ErrorPage from "../Pages/ErrorPage";
+import DocumentoInvalido from "../components/DocumentoInvalido";
+import DocumentoValido from "../components/DocumentoValido";
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
-      element: <Layout />,
+      // element: <Layout />,
       children: [
         {
           path: "/",
           element: <Home />,
         },
-
+        {
+          path: "/documento-invalido", 
+          element: <DocumentoInvalido />,
+        },
+        {
+          path: "/documento-valido",
+          element: <DocumentoValido />,
+        },
         {
           path: "*",
           element: <ErrorPage />,
